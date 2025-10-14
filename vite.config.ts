aa199bf60055c4040741e0ea2664c9c71b9add4d
Ch-jpg-jpg <ayoubdkp@gmail.com>
2025-10-14 14:47:05 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/site-stap-launch/',
   server: {
     host: "::",
     port: 8080,
